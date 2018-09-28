76f4d6a10d5b2c9750f2cb7fbe9ac375213f0034
KangST <cdef3456@naver.com>
2018-09-28 15:46:06 +0900

    delete duplicated file
    delete duplicated file

--- post.py
+++ post.py
@@ -2,15 +2,17 @@ import time
 import requests
 import json
 
+
 def post(createdAt, speaker, speakerId, content):
     try:
-        URL = 'http://155.230.104.191:3001/api/v1/speech'
+        URL = 'http://127.0.0.1:3001/api/v1/speech'
         headers = {'Content-Type': 'application/json; charset=utf-8'}
         data=[('createdAt',createdAt), ('speaker',speaker), ('speakerId',speakerId), ('content',content)]
         res = requests.post(url=URL, data=data)
         print(res.content)
     except:
         pass
+
 
 def post_me(text):
     try:
@@ -22,6 +24,7 @@ def post_me(text):
     except:
         print("error")
         pass
+
 
 if __name__ == '__main__':
     post_me("TEST")
