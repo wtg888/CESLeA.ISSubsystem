528ee249bebcee7a14c81a9d38cb0a32b2b8f164
KangST <cdef3456@naver.com>
2020-01-15 13:57:31 +0900

    change path

--- SpeakerRecogv2/extractor/path.sh
+++ SpeakerRecogv2/extractor/path.sh
@@ -1,8 +1,8 @@
 # The virtualenv path
-export TF_ENV=/home/craftkim/venv
+export TF_ENV=/home/mi/PycharmProjects/CESLeA/venv
 
-export PROJECT_ROOT=/home/craftkim/projects/SpeakerRecog/extractor
-export KALDI_ROOT=/home/craftkim/SD/kaldi-master
+export PROJECT_ROOT=/home/mi/PycharmProjects/CESLeA/SpeakerRecogv2/extractor
+export KALDI_ROOT=/media/mi/data/tools/kaldi
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sph2pipe_v2.5:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
