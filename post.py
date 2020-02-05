c90a465ded8a8b9cd7cbd4e3213794ededbc0094
KangST <cdef3456@naver.com>
2020-02-05 23:19:50 +0900

    change gitignore

--- post.py
+++ post.py
@@ -24,5 +24,49 @@ def post_me(text):
         # pass
 
 
+def post_add_speaker(speaker):
+    try:
+        URL = 'http://x.x.x.x:8080/add_speaker'
+        data=[('speaker', speaker)]
+        res = requests.post(url=URL, data=data)
+        print(res.content)
+    except:
+        print("error")
+
+
+def post_add_data(speaker, audio_file):
+    try:
+        URL = 'http://127.0.0.1:8080/add_data'
+        speaker = io.StringIO(speaker)
+        files = {
+            'audio_file': ('data.mp3', open(audio_file, 'rb')),
+            'speaker': ('name.txt', speaker)
+        }
+        res = requests.post(url=URL, files=files)
+        print(res.content)
+    except:
+        print("error")
+
+
+def post_delete_speaker(speaker):
+    try:
+        URL = 'http://127.0.0.1:8080/delete_speaker'
+        data=[('speaker', speaker)]
+        res = requests.post(url=URL, data=data)
+        print(res.content)
+    except:
+        print("error")
+
+
+def post_delete_data(speaker):
+    try:
+        URL = 'http://127.0.0.1:8080/delete_data'
+        data=[('speaker', speaker)]
+        res = requests.post(url=URL, data=data)
+        print(res.content)
+    except:
+        print("error")
+
+
 if __name__ == '__main__':
     pass
