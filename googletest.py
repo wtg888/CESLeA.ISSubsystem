4ac158beec30afe9da83089bafba1b38deaa5a8f
KangST <cdef3456@naver.com>
2020-02-07 00:03:55 +0900

    Update googletest.py

--- googletest.py
+++ googletest.py
@@ -5,7 +5,7 @@ import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../My First Project-2020.json"
 
 # Imports the Google Cloud client library
-from google.cloud import speech
+from google.cloud import speech, speech_v1p1beta1
 from google.cloud.speech import enums
 from google.cloud.speech import types
 from google.cloud import texttospeech
@@ -60,7 +60,46 @@ def synthesize_text(text, language_code):
     playsound.playsound('output.mp3', True)
 
 
+def google_stt_with_speaker_diarization(file_name, language_code='en-US'):
+    # Instantiates a client
+    client = speech_v1p1beta1.SpeechClient()
+
+    # Loads the audio into memory
+    with io.open(file_name, 'rb') as audio_file:
+        content = audio_file.read()
+
+    audio = {"content": content}
+
+    config ={
+        'encoding' : enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        'enable_speaker_diarization' : True,
+        'diarization_speaker_count' : 3,
+        'language_code' : language_code
+    }
+
+    try:
+        response = client.recognize(config, audio)
+        res = list()
+        prev_spk = -1
+        string = ''
+        for result in response.results:
+            alternative = result.alternatives[0]
+            # print(alternative)
+            for word in alternative.words:
+                if word.speaker_tag == prev_spk:
+                    string += ' ' + word.word
+                else:
+                    if prev_spk != -1:
+                        res.append((prev_spk, string))
+                    string = word.word
+                prev_spk = word.speaker_tag
+        res.append((prev_spk, string))
+        return res
+    except:
+        print("error")
+        return "error"
+
+
 if __name__ == "__main__":
-    res = google_stt('file path')
+    res = google_stt_with_speaker_diarization('test.wav')
     print(res)
-    # synthesize_text("Hello!!", 'en-US')
