a20dd75fa3eb1e8ac5cfb7f27aae8de5286c92d8
lsw0767 <lsw0767@gmail.com>
2020-08-12 16:49:36 +0900

    Update recording_gui.py

--- live_recording_test/recording_gui.py
+++ live_recording_test/recording_gui.py
@@ -8,7 +8,7 @@ import os
 import sys
 import datetime
 from tkinter import *
-os.chdir('..')
+# os.chdir('..')
 
 from data_split.vad_on_splited_data import preprocess
 # from speaker_recog.predict_speaker_recog import predict_speaker
