26fd32513def267a0bc1b835b547aace3976ee3a
Ckst123 <38306760+Ckst123@users.noreply.github.com>
2020-08-13 10:59:42 +0900

    Update utils.py

--- STT_test/utils.py
+++ STT_test/utils.py
@@ -1,2 +1,10 @@
 import numpy as np
 import os
+
+def load_np_file(file_path):
+    data = np.load(file_path, allow_pickle=True)
+    return data
+
+if __name__ == '__main__':
+    data = load_np_file('test.npy')
+    print(data)
