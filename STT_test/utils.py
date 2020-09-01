0f5c11a3d7ba00ff221db11b66adae71fc3c8bd6
Ckst123 <38306760+Ckst123@users.noreply.github.com>
2020-09-01 10:12:41 +0900

    Update utils.py

--- STT_test/utils.py
+++ STT_test/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 
 def load_np_file(file_path):
-    data = np.load(file_path, allow_pickle=True)
+    data = np.load(file_path)
     return data
 
 if __name__ == '__main__':
