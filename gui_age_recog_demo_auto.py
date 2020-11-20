faaff9bc0e55418ca7b9562a05a4a1338f450be6
KangST <cdef3456@naver.com>
2020-11-20 17:26:00 +0900

    chang model

--- gui_age_recog_demo_auto.py
+++ gui_age_recog_demo_auto.py
@@ -11,7 +11,7 @@ import pyaudio
 from tkinter import *
 import shutil
 
-import age_recog_v2
+import speaker_recog_v2 as age_recog_v2
 import requests
 
 On = True
@@ -25,7 +25,7 @@ def post_res(spk):
     res = requests.post(URL, data={'text': spk})
 
 
-def write_wave(path, audio, sample_rate):
+def write_wave(path, audio, sample_rate=16000):
     with contextlib.closing(wave.open(path, 'wb')) as wf:
         wf.setnchannels(1)
         wf.setsampwidth(2)
@@ -33,67 +33,39 @@ def write_wave(path, audio, sample_rate):
         wf.writeframes(audio)
 
 
-def vad_thread(sample_rate, frame_duration_ms, padding_duration_ms, vad, stream):
+def record_thread(sample_rate, frame_duration_ms, length_ms, inference_ms, stream):
     global On
-    num_padding_frames = int(padding_duration_ms / frame_duration_ms)
     chunk = int(sample_rate * (frame_duration_ms / 1000.0))
-    ring_buffer = collections.deque(maxlen=num_padding_frames)
-    triggered = False
+    ring_buffer = collections.deque(maxlen=length_ms // frame_duration_ms)
+
     num = 0
-    voiced_frames = []
     try:
         while True:
             frame = stream.read(chunk)
-            is_speech = vad.is_speech(frame, sample_rate)
-            if not triggered:
-                ring_buffer.append((frame, is_speech))
-                num_voiced = len([f for f, speech in ring_buffer if speech])
-                if On and len(ring_buffer) == ring_buffer.maxlen and num_voiced > 0.9 * ring_buffer.maxlen:
-                    print('on')
-                    triggered = True
-                    for f, s in ring_buffer:
-                        voiced_frames.append(f)
-                    ring_buffer.clear()
-            else:
-                if not On:
-                    triggered = False
-                    ring_buffer.clear()
-                    voiced_frames = []
-                    continue
-                voiced_frames.append(frame)
-                ring_buffer.append((frame, is_speech))
-                num_unvoiced = len([f for f, speech in ring_buffer if not speech])
-                if len(ring_buffer) == ring_buffer.maxlen and num_unvoiced > 0.9 * ring_buffer.maxlen:
-                    print('off')
-                    triggered = False
-                    data = b''.join([f for f in voiced_frames])
-                    fn = os.path.join('wavfile', '%d.wav'% num)
-                    write_wave(fn, data, sample_rate)
-                    now = int(time.time())
-                    q.put_nowait((now, fn))
-                    num = num + 1
-                    ring_buffer.clear()
-                    voiced_frames = []
+            ring_buffer.append(frame)
+
+            if len(ring_buffer) == ring_buffer.maxlen:
+                num += 1
+                if num == inference_ms // frame_duration_ms:
+                    num = 0
+                    data = b''.join([f for f in ring_buffer])
+                    q.put_nowait(data)
+
     except:
         pass
 
 
 def speaker_recog_thread(outLabel):
     global d
-    global On
     while True:
         try:
-            g = q.get()
-            On = False
-            now, file_name = g
-            now_s = str(now)
-            outLabel.config(text='이름')
-            shutil.copy(file_name, os.path.join(age_recog_v2.DATA_DIR, 'test', 'test.wav'))
+            data = q.get()
+
+            write_wave(os.path.join(age_recog_v2.DATA_DIR, 'test', 'test.wav'), data)
             speaker = age_recog_v2.test_speaker_recog()
             outLabel.config(text=speaker)
             post_res(speaker)
-            print(now_s, speaker)
-            On = True
+            print(speaker)
         except queue.Empty:
             continue
 
@@ -101,13 +73,11 @@ def speaker_recog_thread(outLabel):
 def main():
     age_recog_v2.load_speaker_list()
     RATE = 16000
-    frame_duration_ms = 30
+    frame_duration_ms = 10
     CHUNK = int(RATE * (frame_duration_ms / 1000.0))
     FORMAT = pyaudio.paInt16
     CHANNELS = 1
 
-    if not os.path.isdir('wavfile'):
-        os.mkdir('wavfile')
 
     p = pyaudio.PyAudio()
 
@@ -116,8 +86,6 @@ def main():
                     rate=RATE,
                     input=True,
                     frames_per_buffer=CHUNK)
-
-    vad = webrtcvad.Vad(1)  # 0~3   3: the most aggressive
 
     root = Tk()
     root.geometry("200x200")
@@ -128,7 +96,7 @@ def main():
     lbl.config(font=("Courier", 44))
     lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-    t1 = threading.Thread(target=vad_thread, args=(RATE, frame_duration_ms, 300, vad, stream))
+    t1 = threading.Thread(target=record_thread, args=(RATE, frame_duration_ms, 3000, 3000, stream))
     t2 = threading.Thread(target=speaker_recog_thread, args=(lbl,))
     t1.daemon = True
     t2.daemon = True
