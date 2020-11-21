59d691939ba96b61e5ed23516688ac55bd81abfe
KangST <cdef3456@naver.com>
2020-11-21 22:32:05 +0900

    Update asr_auto.py

--- asr_auto.py
+++ asr_auto.py
@@ -1,11 +1,9 @@
-import asyncio
-import pathlib
-import ssl
-import websockets
-
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
-ssl_context.load_verify_locations(localhost_pem)
+import websocket
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
 
 
 def on_message(ws, message):
@@ -20,17 +18,22 @@ def on_close(ws):
     print("### closed ###")
 
 
-async def hello():
-    uri = "wss://localhost:8765"
-    async with websockets.connect(
-        uri, ssl=ssl_context
-    ) as websocket:
-        name = input("What's your name? ")
+def on_open(ws):
+    def run(*args):
+        for i in range(3):
+            time.sleep(1)
+            ws.send("Hello %d" % i)
+        time.sleep(1)
+        ws.close()
+        print("thread terminating...")
+    thread.start_new_thread(run, ())
 
-        await websocket.send(name)
-        print(f"> {name}")
 
-        greeting = await websocket.recv()
-        print(f"< {greeting}")
-
-asyncio.get_event_loop().run_until_complete(hello())
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://echo.websocket.org/",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
